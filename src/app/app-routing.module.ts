ecf306b8ecd8b310f6581ad51733a760ae7171b0
VladanStar <vladancupric@gmail.com>
2023-03-26 00:05:34 +0100

    Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 
 const routes: Routes = [
   {path:"", redirectTo:"login", pathMatch:"full"},
   {path:"login", component:LoginComponent},
   {path:"register",component:RegisterComponent},
+  { path: 'verify-email', component: VerifyEmailComponent },
   {path:"main",component:MainComponent}
 ];
 
