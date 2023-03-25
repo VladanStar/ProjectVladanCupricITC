cccdcfc5db09e826d68e045b33d7a148c0325673
VladanStar <vladancupric@gmail.com>
2023-03-25 23:54:03 +0100

    Add

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,39 +4,32 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  email : string = '';
-  password : string = '';
+  email: string = '';
+  password: string = '';
 
-  constructor(private auth : AuthService) {
+  constructor(private auth: AuthService) {
     // this.auth.updateLoginStatus(true);
-   }
-
-  ngOnInit(): void {
   }
 
-  login() {
+  ngOnInit(): void {}
 
-    if(this.email == '') {
+  login() {
+    if (this.email == '') {
       alert('Please enter email');
       return;
     }
 
-    if(this.password == '') {
+    if (this.password == '') {
       alert('Please enter password');
       return;
     }
 
-    this.auth.login(this.email,this.password);
+    this.auth.login(this.email, this.password);
 
     this.email = '';
     this.password = '';
-
   }
-
- 
-
-
 }
