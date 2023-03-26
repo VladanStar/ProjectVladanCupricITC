7974977c1b870dc324416b8a1f0f6af15333248c
VladanStar <vladancupric@gmail.com>
2023-03-26 12:01:11 +0200

    Add

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css']
 })
-export class AddTaskComponent {
+export class AddTaskComponent  implements OnInit{
+  constructor(private taskService: TaskService, private router: Router){}
+  ngOnInit(): void {
+    
+  }
+  
+addTask(f: NgForm) {
+  // console.log(f.value)
+
+  this.taskService.add(f.value);
+
+  window.alert('Task je dodat');
+  this.router.navigate(['main']);
+}
 
 }
