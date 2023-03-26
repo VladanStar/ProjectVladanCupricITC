fc8597b050e60c00ffa3ec91f42b0a8aa951c552
VladanStar <vladancupric@gmail.com>
2023-03-26 10:26:23 +0200

    Add

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Observable, map } from 'rxjs';
+import { Task } from '../models/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  
+  getAll(): Observable<Task[]> {
+    return this.db
+      .list<Task>('/tasks')
+      .snapshotChanges()
+      .pipe(
+        map((x) =>
+          x.map((y: any) => ({
+            id: y.payload.key,
+            ...(y.payload.val() as Task),
+          }))
+        )
+      );
+  }
+  get(id: string): Observable<Task> {
+    return this.db
+      .object<Task>('/tasks/' + id)
+      .snapshotChanges()
+      .pipe(
+        map((x: any) => ({
+          id: x.payload?.key,
+          ...(x.payload.val() as Task),
+        }))
+      );
+  }
+  update(taskId: string, Task: Task): void {
+    this.db.object<Task>('/tasks/' + taskId).update(Task);
+  }
+  add(Task: Task) {
+    this.db.list('/tasks').push(Task);
+  }
+  delete(taskId: any) {
+    this.db.object<Task>('/tasks/' + taskId).remove();
+  }
+}
